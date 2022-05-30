c67241ddf6807a29483d4371c0b0b35b8dfe5397
Rufat <veliev.rufat@mail.ru>
2022-05-30 22:41:28 +0400

    fix cli script

--- Makefile
+++ Makefile
@@ -18,7 +18,4 @@ check: selfcheck test test-coverage lint
 build:
 	poetry build
 
-gendiff:
-	poetry run gendiff
-
 .PHONY: install test test-coverage lint selfcheck check build
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ import argparse
 from gendiff.generate_diff import generate_diff
 
 
-def main(args=None):
+def main():
     parser = argparse.ArgumentParser(
         prog='gendiff',
         description='Compares two configuration files and shows a difference.',
@@ -12,17 +12,16 @@ def main(args=None):
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument(
-        '--f',
+        '-f',
         '--format',
-        dest='FORMAT',
-        default='stylish',
         help='set format of output',
+        default='stylish',
     )
-    parsed_args = parser.parse_args(args)
+    args = parser.parse_args()
     diff = generate_diff(
-        parsed_args.first_file,
-        parsed_args.second_file,
-        parsed_args.format,
+        args.first_file,
+        args.second_file,
+        args.format,
     )
     print(diff)
 
